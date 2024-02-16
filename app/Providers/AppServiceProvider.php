a9a89ff7bfaa91b7f626a767d29b0f751eb79d25
Sylvain Schaufelberger <sylvain.schaufelberger@unisante.ch>
2024-02-16 17:34:47 +0200

    add force https to URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (!$this->app->isLocal()) {
+            URL::forceScheme('https');
+        }
+
         Paginator::useBootstrapFive();
     }
 }
